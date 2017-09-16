f7d09164b5107e20f639ca9e25ce8ecc99d514f6
jbk77 <josephbk.117@hotmail.com>
2017-09-16 12:57:29 +0530

    adding vertex selection capability

--- Open GL Assignment 2/Model.cpp
+++ Open GL Assignment 2/Model.cpp
@@ -14,32 +14,46 @@ void Model::updateMeshData()
 {
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, vertexData.size() * sizeof(float), vertexData.data(), GL_DYNAMIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, vertexData.size() * 3 * sizeof(float), vertexData.data(), GL_DYNAMIC_DRAW);
 	// position attribute
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
-void Model::addVertex(glm::vec3 vertexPosition)
+vec3 * Model::vertexAtViewportCoord(float x, float y, float z)
 {
-	vertexData.push_back(vertexPosition.x);
-	vertexData.push_back(vertexPosition.y);
-	vertexData.push_back(vertexPosition.z);
+	for (int i = 0; i < vertexData.size(); i++)
+	{
+		if (vertexData[i].x > x - 10 && vertexData[i].x < x + 10)
+		{
+			if (vertexData[i].y > y - 10 && vertexData[i].y < y + 10)
+			{
+				if (vertexData[i].z > z - 10 && vertexData[i].z < z + 10)
+				{
+					return &vertexData[i];
+					break;
+				}
+			}
+		}
+	}
+}
+
+void Model::addVertex(vec3 vertexPosition)
+{
+	vertexData.push_back(vertexPosition);
 }
 
 void Model::addVertex(float x, float y, float z)
 {
-	vertexData.push_back(x);
-	vertexData.push_back(y);
-	vertexData.push_back(z);
+	vertexData.push_back(vec3(x, y, z));
 }
 
 void Model::display()
 {
 	glPointSize(5);
 	glBindVertexArray(VAO);
-	glDrawArrays(GL_POINTS, 0, vertexData.size() / 3);
-	glDrawArrays(GL_LINE_STRIP, 0, vertexData.size() / 3);
+	glDrawArrays(GL_POINTS, 0, vertexData.size());
+	glDrawArrays(GL_LINE_STRIP, 0, vertexData.size());
 	glBindVertexArray(0);
 }
--- Open GL Assignment 2/Model.h
+++ Open GL Assignment 2/Model.h
@@ -3,18 +3,21 @@
 #include <iostream>
 #include <GL\glew.h>
 #include <GLM\glm.hpp>
+using namespace glm;
+
 class Model
 {
 public:
 	Model();
 	~Model();
-	void addVertex(glm::vec3 vertexPosition);
+	void addVertex(vec3 vertexPosition);
 	void addVertex(float x, float y, float z);
 	void display();
 	void updateMeshData();
+	vec3* vertexAtViewportCoord(float x, float y, float z);
 private:
 	unsigned int VBO, VAO;
-	std::vector<float> vertexData;
+	std::vector<vec3> vertexData;
 
 };
 
